8f42695bbc06fbcf4a7dc04440cdb89de916e272
arfat-beep <arfatrahman08@gmail.com>
2022-04-29 12:07:40 +0600

    Mongo Db ADDED

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -8,6 +9,28 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// Db connection
+const uri = `mongodb+srv://${process.env.DB_name}:${process.env.DB_pass}@cluster0.mahcw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const productCollection = client.db("cars-inventory").collection("products");
+
+const run = async () => {
+  try {
+    await client.connect();
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+};
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("This port is running by localhost 5000");
 });
